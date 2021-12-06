d29564ae31de9f2ef47dedfee9c8d2079bd71be4
Brenda Ramos Bezerra <brendaramos.bezerra@gmail.com>
2021-12-06 15:56:13 -0300

    alterei arquivo x - fake message

---
+++ app.js
@@ -0,0 +1,69 @@
+const buttonElement = document.getElementById("btn");
+
+buttonElement.addEventListener("click", function (event) {
+    document.getElementById("name").innerHTML = "Meu nome é Brenda.";
+})
+
+
+
+
+
+
+
+
+
+//console.log (buttonElement);
+// document.getElementById("name").innerHTML = "Brenda Ramos Bezerra"; => ao fazer isso, o nome aparece logo abaixo do título. Essa etapa foi feita antes de escrever a função 'addEventListener'.
+
+// const myName="Brenda";
+// const myAge="25 anos";
+// const nicePerson = true;
+// const bee__3insects ="related to bees";
+
+// console.log (myName)
+// console.log (myAge)
+// console.log (nicePerson)
+// console.log (bee__3insects)
+
+// soma = +
+// subtração = -
+// divisão = /
+// multiplicação = *
+// resto = %
+// maior que = >
+// menor que = <
+// igual = ===
+
+// let resultado= 10 + 10;
+// console.log ("Soma: ", resultado);
+
+// subtracao= 10 - 10;
+// console.log ("Resultado: ", subtracao);
+
+// resultado= 10 - 10;
+// console.log ("Subtracao: ", resultado);
+
+// let resultado2= 10 * 10;
+// console.log ("Multiplicacao: ", resultado2);
+
+// resultado = 4 > 5;
+// console.log ("4 é maior que 5", resultado);
+
+// resultado = 4 < 5;
+// console.log ("4", resultado);
+
+// const valor1= 20;
+// const valor2= 10;
+
+//if (valor1 != valor2 ) {
+//    console.log("valores são diferentes");
+//} else {
+//     console.log("valores são iguais");
+//}
+
+// if (valor1 - valor2 === 10) {
+//     console.log("valor é igual a 10");
+//} else {
+//    console.log("valore é diferente de 10");
+//}
+
